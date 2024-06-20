f3852a4655d66f97701840c1a42da1f681061110
IBLINETUM\juan-pedro.sanchez <juan-pedro.sanchez@inetum.com>
2024-06-20 14:13:02 +0200

    Initial commit

---
+++ src/main/java/funanaco/thymeleaf/FunanacoThymeleafApplication.java
@@ -0,0 +1,13 @@
+package funanaco.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunanacoThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunanacoThymeleafApplication.class, args);
+	}
+
+}
